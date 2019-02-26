ee784722a127f5a58b6d60c78b0e4ad2f1ab352d
ivantaimal <ivan.taimal@bizagi.com>
2019-02-26 17:38:50 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+### Console translate
+
+Do you like commands? Now you can use translate without using the browser
+
+Install:
+```
+npm install
+```
+
+Examples:
+```
+node translate "Está lloviendo"
+node translate "hello world" sk
+node translate "hello world" es
+```
---
+++ translate.js
@@ -0,0 +1,19 @@
+const translate = require('./src/index');
+const languages = require('./src/languages');
+const arguments = process.argv;
+let phrase, targetLanguage;
+if (arguments.length > 2) {
+    phrase = arguments[2];
+    targetLanguage = arguments[3];
+    translate(phrase, {to: targetLanguage || 'en'})
+        .then(res => {
+            console.log(`${phrase}  ${res.from.language.iso}->${res.to.language.iso}   ${res.text}`);
+        }).catch(err => {
+        console.error(err);
+    });
+
+} else {
+    console.log("you must use: >   node translate \"Your text\"");
+    console.log("Languages: ", languages);
+}
+
